9bc8d8a046e9be12b3af0824c30eef244e527499
MoayadHakim <moayad.hakim@gmail.com>
2024-09-14 00:28:06 +0300

    Add the project

---
+++ build/app/generated/source/buildConfig/debug/com/example/sqlite_flutter_crud/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.sqlite_flutter_crud;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.sqlite_flutter_crud";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
